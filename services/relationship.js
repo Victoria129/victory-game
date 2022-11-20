e844d5603f00e54caff9521b6ca3193ceaaeb9d5
Victoria129 <victoria.tumwebaze@outlook.com>
2022-11-20 14:48:09 +0300

    add constructor

--- services/relationship.js
+++ services/relationship.js
@@ -1,6 +1,29 @@
 class Relationship {
+  constructor() {
+    this._relationship = {};
+  }
   friendship(id) {
     return 0;
+  }
+
+  improve(characterId) {
+
+    // this._relationship[1] = this._relationship[1]+ 1;
+
+    let result = this._relationship[1];
+    if(result === undefined){
+     this._relationship[this._relationship[1]];
+    }
+    
+    return this._relationship[1];
+  }
+
+  ruine(characterId) {
+
+  }
+
+  assess(characterId, direction) {
+
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,28 @@
 const Relationship = require('../services/relationship')
 
 
-const relashionship = new Relationship();
+const relationship = new Relationship();
 
 test('Are they friends', () => {
   //Assign
   const expected = 0;
 
   //Act
-  const actual = relashionship.friendship(2);
+  const actual = relationship.friendship(2);
 
+
+  //Assert
+  expect(actual).toBe(expected);
+})
+
+test('Is our relashionship improved?', () => {
+  //Assign
+  const expected = 2;
+
+  //Act
+  relationship.improve(1);
+  const actual = relationship.improve(1);
+  
 
   //Assert
   expect(actual).toBe(expected);
